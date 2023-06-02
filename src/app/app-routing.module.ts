b23bab4c653f06759bbe2ad25f320d8d5bcdc9e5
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-06-02 16:50:47 +0300

    added routing for employee list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './Employee/employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employees', component: EmployeeListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
