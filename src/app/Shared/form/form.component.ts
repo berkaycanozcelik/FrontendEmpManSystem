ceb89678a44fe0735a45d797c94358b5b056bdd8
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-06-02 21:36:27 +0300

    adjusted onSubmit

--- src/app/Shared/form/form.component.ts
+++ src/app/Shared/form/form.component.ts
@@ -1,18 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Employee } from 'src/app/Employee/employee';
+import { EmployeeService } from 'src/app/Employee/employee.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
 })
-export class FormComponent {
-  formData = {
-    name: '',
-    surname: '',
-    email: '',
-  };
+export class FormComponent implements OnDestroy {
+  private createEmployeeSubscription: Subscription | undefined;
 
-  onSubmit() {
-    console.log(this.formData);
+  formData: Employee = {
+    firstName: '',
+    lastName: '',
+    emailId: '',
+  };
+  constructor(private employeeService: EmployeeService) {}
+
+  onSubmit(): void {
+    this.createEmployeeSubscription = this.employeeService
+      .createEmployee(this.formData)
+      .subscribe(
+        (response) => {
+          // Handle success response here
+          console.log('Employee created:', response);
+        },
+        (error) => {
+          // Handle error response here
+          console.error('Error creating employee:', error);
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    if (this.createEmployeeSubscription) {
+      this.createEmployeeSubscription.unsubscribe();
+    }
   }
 }
