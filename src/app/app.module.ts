04523467242e3b0e3f06ef0e26ccfcb4b08d3d4a
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-06-02 17:08:41 +0300

    created navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './Employee/employee-list/employee-list.component';
+import { NavbarComponent } from './Shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
