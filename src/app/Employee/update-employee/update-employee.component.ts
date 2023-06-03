f99dc9f5b03e6d3ea60a44cb821625561813d088
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-06-03 13:48:32 +0300

    getting the user Data in the form through the id

--- src/app/Employee/update-employee/update-employee.component.ts
+++ src/app/Employee/update-employee/update-employee.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-update-employee',
   templateUrl: './update-employee.component.html',
-  styleUrls: ['./update-employee.component.css']
+  styleUrls: ['./update-employee.component.css'],
 })
 export class UpdateEmployeeComponent {
+  employee: Employee = new Employee();
 
+  constructor(
+    private employeeService: EmployeeService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const employeeId = +this.route.snapshot.params['id'];
+    this.employeeService.getEmployeeById(employeeId).subscribe((data) => {
+      this.employee = data;
+    });
+  }
 }
