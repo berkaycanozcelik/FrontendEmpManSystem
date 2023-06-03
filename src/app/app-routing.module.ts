92fa56145a1429cd596f508f9873589a576af1f1
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-06-03 13:12:28 +0300

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './Employee/employee-list/employee-list.component';
 import { CreateEmployeeComponent } from './Employee/create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './Employee/update-employee/update-employee.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/employees', pathMatch: 'full' },
   { path: 'employees', component: EmployeeListComponent },
   { path: 'create-employee', component: CreateEmployeeComponent },
+  { path: 'update-employee/:id', component: UpdateEmployeeComponent },
 ];
 
 @NgModule({
